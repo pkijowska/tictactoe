20a678f8f5d392f552ccf5079b82584f6a859ae4
Paulina <p.kijowska@gmail.com>
2019-07-09 21:03:58 +1000

    added button etc

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -30,12 +30,9 @@ let playerTwoCombos = [];
 $('.box').one("click", function () {
 if (playerOne) {
 
-
-
 const img = $('<img id="img1">'); //Equivalent: $(document.createElement('img'))
 img.attr('src', "https://media1.giphy.com/media/3xz2BCohVTd7h2Kvfi/200.webp?cid=790b76115d22c46545414a44552b6e55&rid=200.webp");
 img.appendTo(this);
-
 
 
   //console.log($(this).attr("id"));
@@ -51,7 +48,7 @@ playerOneCombos.push($(this).attr("id"));
 } else {
 
   const img = $('<img id="img2">'); //Equivalent: $(document.createElement('img'))
-  img.attr('src', "https://media0.giphy.com/media/23xN9cYQSKwFy/200w.webp?cid=790b76115d22c4df78505038595e6040&rid=200w.webp");
+  img.attr('src', "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpspo-w1z4Mpro3XaphOs6Xs8NZKf9SyzPdGWBW2p-alr2ID5oKg");
   img.appendTo(this);
 
    playerTwoCombos.push($(this).attr("id"));
@@ -61,21 +58,68 @@ playerOneCombos.push($(this).attr("id"));
   //console.log('playerTwoCombos', playerTwoCombos);
 }
 
-if (playerOneCombos.length === 5 && playerTwoCombos.length ===4) {
+if ((playerOneCombos.length === 5 && playerTwoCombos.length ===4) ||
+(playerTwoCombos.length === 5 && playerOneCombos.length ===4))
+
+ {
   //.offclick
 
   const hedge = timetocheck(playerOneCombos);
   const dog = timetocheck(playerTwoCombos);
+  const dogpoints = 0;
+  const hedgepoints = 0;
   //console.log("winner 1");
   if (hedge) {
     console.log("player 1 wins");
     $("h2").removeClass("player1");
+    $("body").addClass("animated flash delay-2s");
+    $("h2").addClass("animated bounceInUp delay-1s");
+    //counting points for winning
+    const newdogpoints = dogpoints + 1;
+    console.log(newdogpoints);
+    const newhedge = $("#teamhedge").text();
+    const finalhedge = +newhedge+dogpoints;
+    console.log(finalhedge);
+
+
+
   }
   if (dog) {
     console.log("player 2 wins");
     $("h2").removeClass("player2");
+    //$("table").addClass("animated hinge delay-2s");
+    $("h2").addClass("animated bounceInUp delay-1s");
+//counting points for winning
   }
+// if (hedge && dog) {
+//   console.log("draw");
+//   $("h2").addClass("player1");
+//   $("h2").addClass("player2");
+//   $("h2").removeClass("draw");
+// }
+
+if (hedge && dog) {
+  console.log("draw");
+  $("h2").removeClass("draw");
+  $("h2").addClass("player1");   ///the code doesn't work with that line
+ $("h2").addClass("player2");     ////code doesnt work with that line
 }
+
+
+
+if (!hedge && !dog) {
+  console.log("draw");
+  $("h2").removeClass("draw");
+  $("body").addClass("animated wobble");
+}
+
+
+
+}
+
+$('#button').click(function() {
+    location.reload();
+});
 
 });
 
@@ -94,7 +138,7 @@ const timetocheck = function (array) {
   (array.includes("four") && array.includes("five") && array.includes("six")) ||
   (array.includes("seven") && array.includes("eight") && array.includes("nine")) ||
   (array.includes("one") && array.includes("four") && array.includes("seven")) ||
-  (array.includes("two") && array.includes("five") && array.includes("six")) ||
+  (array.includes("two") && array.includes("five") && array.includes("eight")) ||
   (array.includes("three") && array.includes("six") && array.includes("nine")) ||
   (array.includes("one") && array.includes("five") && array.includes("nine")) ||
   (array.includes("three") && array.includes("five") && array.includes("seven")) ) {
