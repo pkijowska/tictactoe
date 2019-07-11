d0ab491941f51a8f533a3839a1f50c7e52430669
Paulina <p.kijowska@gmail.com>
2019-07-11 20:33:53 +1000

    all cleaned up

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Project0 Tic Tac Toe
 
+Play the classic Tic Tac Toe game!
 
+This game is FULL of css effects that make the page look really cool.
 
 
 
@@ -10,16 +12,20 @@
 
 
 ## Bugs
-
+None I am aware of.
 
 ## TODO List
 1. An unbeatable AI
-1. blabla
+1. Make the bubble faces look nicer
+1. Make a module that will enable players to pick the the bubble they want to be
+
 
 ## Technologies
-- Made with jquery
+- Made with jquery, javascript, css
 - Uses animate css
+
 
 ## Thank you
 
 1. Rashida for helping me several times when I got stuck
+1. Lucy for making the eyes work   (◔_◔)
--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,30 +1,8 @@
 
 
-let score = 0;
-const increment = function() {
-score += 1;
-return score;
-};
 
 
 $(document).ready(function () {
-
-// prompt("What is your name?");
-//
-// alert("Pick your picture");
-
-// $(".one").click(function(){
-//   $("td:first").addClass("pic");
-
-// const randomnum = Math.floor(Math.random() * 7);
-
-// const tiles = ["#zero", "#one", "#two", "#three", "#four", "#five", "#six", "#seven", "#eight"];
-// let index = tiles.indexOf(tiles[randomnum]);
-// if (index > -1) {
-//   tiles.splice(index, 1);
-// }
-// console.log(tiles);
-
 
 let playerOne = true;
 let playerTwo = false;
@@ -33,71 +11,59 @@ let playerTwoCombos = [];
 
 $('.box').one("click", function () {
 if (playerOne) {
-// const div = $(".move-area");
-// console.log('div', div);
 const eyeBox = $(this).append(`
 <div class="move-area">
     <div class='eye'></div>
     <div class='eye'></div>
     <div class='mouth'></div>
-    </div>
-`);
+    </div>`);
 // $('.move-area').appendTo(this);
-//$(this).html($('.move-area'));
-console.log($('.move-area'));
-//console.log('.face1');
-console.log(this);
-// $(this).html(newdiv);
-// $(this).html(newdiv);
+
+console.log('eyeBox', eyeBox);
+
+// const bleep = new Audio();
+
 moveeyes();
-// })
 
-  //console.log($(this).attr("id"));
-
-  //this.offclick
 playerOneCombos.push($(this).attr("id"));
 
-// $('.box').text($(this).attr('id'));
+
   playerOne = false;
   playerTwo = true;
-  console.log('playerOneCombos', playerOneCombos);
 
 
-
-  //decideWinner(playerOneCombos)
 } else {
 
 
   const eyeBoxes = $(this).append(`
-    <div class="movingeyes">
-  <div class="player2img">
-
-    <div class='eye2'></div>
-      <div class='eye2'></div>
-
-  </div>
+    <div class="move2">
+        <div class='eye2'></div>
+        <div class='eye2'></div>
+        <div class='mouth2'>X</div>
+        </div>
   `);
-  // const img = $('<img id="img2">'); //Equivalent: $(document.createElement('img'))
-  // img.attr('src', "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpspo-w1z4Mpro3XaphOs6Xs8NZKf9SyzPdGWBW2p-alr2ID5oKg");
-  // img.appendTo(this);
+
+  console.log('eyeBoxes', eyeBoxes);
+  moveeyes2();
 
    playerTwoCombos.push($(this).attr("id"));
-   console.log('playerTwoCombos', playerTwoCombos);
   playerOne = true;
   playerTwo = false;
-  //console.log('playerTwoCombos', playerTwoCombos);
+
 }
 
-// if ((playerOneCombos.length === 3 && playerTwoCombos.length === 2 ) ||
-// (playerOneCombos.length === 2 && playerTwoCombos.length === 3 ))
-//  {
-//   //.offclick
 
-  const hedge = timetocheck(playerOneCombos);
-  const dog = timetocheck(playerTwoCombos);
+let score = 0;
+const increment = function() {
+score += 1;
+return score;
+};
+
+  const bub1 = timetocheck(playerOneCombos);
+  const bub2 = timetocheck(playerTwoCombos);
 
   //console.log("winner 1");
-  if (hedge) {
+  if (bub1) {
     console.log("player 1 wins");
     $("h2").removeClass("player1");
     $(".container").addClass("animated flash delay-2s");
@@ -108,12 +74,12 @@ playerOneCombos.push($(this).attr("id"));
     //adding the points to the table
   //  $("#button").click(function() {
       const getIncreasedValue = increment();
-      $("#teamhedge").text(getIncreasedValue)
+      $("#teamplay1").text(getIncreasedValue)
         $("td").off("click");
   //  });
   }
 
-  if (dog) {
+  if (bub2) {
     console.log("player 2 wins");
     $("h2").removeClass("player2");
     //$("table").addClass("animated hinge delay-2s");
@@ -121,56 +87,13 @@ playerOneCombos.push($(this).attr("id"));
       $("table").addClass("tables");
 //counting points for winning
 const getIncreasedValues = increment();
-$("#teamdoggo").text(getIncreasedValues)
+$("#teamplay2").text(getIncreasedValues)
 $("td").off("click");
   }
-// if (hedge && dog) {
-//   console.log("draw");
-//   $("h2").addClass("player1");
-//   $("h2").addClass("player2");
-//   $("h2").removeClass("draw");
-// }
-//
-// } else if ((playerOneCombos.length === 4 && playerTwoCombos.length === 3 ) ||
-// (playerOneCombos.length === 3 && playerTwoCombos.length === 3 )) {
-//
-//   if (hedge) {
-//     console.log("player 1 wins");
-//     $("h2").removeClass("player1");
-//     $(".container").addClass("animated flash delay-2s");
-//     $("h2").addClass("animated bounceInUp delay-1s");
-//     //counting points for winning
-//
-//     //adding the points to the table
-//   //  $("#button").click(function() {
-//       var getIncreasedValue = increment();
-//       $("#teamhedge").text(getIncreasedValue)
-//   //  });
-//   }
-//
-//   if (dog) {
-//     console.log("player 2 wins");
-//     $("h2").removeClass("player2");
-//     //$("table").addClass("animated hinge delay-2s");
-//     $("h2").addClass("animated bounceInUp delay-1s");
-//   //counting points for winning
-//   var getIncreasedValue = increment();
-//   $("#teamdog").text(getIncreasedValue)
-//   }
-//
-// }
 
-// if (hedge && dog) {
-//   console.log("draw");
-//   $("h2").removeClass("draw");
-//   $("h2").addClass("player1");   ///the code doesn't work with that line
-//  $("h2").addClass("player2");     ////code doesnt work with that line
-// }
-//
-//
-//
+
 if (playerOneCombos.length === 5 && playerTwoCombos.length === 4 ) {
-  if (!hedge && !dog) {
+  if (!bub1 && !bub2) {
     console.log("draw");
     $("h2").removeClass("draw");
     $("table").addClass("tables");
@@ -181,37 +104,16 @@ if (playerOneCombos.length === 5 && playerTwoCombos.length === 4 ) {
 
 
 
-// $(".move-area").mousemove(function(event) {
-//   var eye = $(".eye");
-//   var x = (eye.offset().left) + (eye.width() / 2);
-//   var y = (eye.offset().top) + (eye.height() / 2);
-//   var rad = Math.atan2(event.pageX - x, event.pageY - y);
-//   var rot = (rad * (180 / Math.PI) * -1) + 180;
-//   eye.css({
-//     '-webkit-transform': 'rotate(' + rot + 'deg)',
-//     '-moz-transform': 'rotate(' + rot + 'deg)',
-//     '-ms-transform': 'rotate(' + rot + 'deg)',
-//     'transform': 'rotate(' + rot + 'deg)'
-//   });
-// });
 
 
+//reloading the page button
 $('#button').click(function() {
     location.reload();
 });
 
 });
 
-// const timetocheck = function() {
-// if ((playerOneCombos.length === 3) && (playerOneCombos === option1)) {
-//   console.log("winner");
-//
-// }else {
-// console.log("do not give up");
-// }
-// }
-// timetocheck();
-
+//all options to win
 const timetocheck = function (array) {
   if ( (array.includes("one") && array.includes("two") && array.includes("three")) ||
   (array.includes("four") && array.includes("five") && array.includes("six")) ||
@@ -227,7 +129,7 @@ const timetocheck = function (array) {
 
 
 
-//update the score if player won
+//moveeyes1 function
 function moveeyes () {
   const eyeBoxArray = $(".move-area");
 
@@ -248,10 +150,37 @@ function moveeyes () {
         'transform': 'rotate(' + rot + 'deg)'
       });
     });
-  }
-}
+  };
 
-moveeyes();
+};
+
+//moving eyes function
+
+function moveeyes2 () {
+  const eyeboxArray = $(".move2");
+
+  eyeboxArray.each(moveEyesAgain);
+
+  function moveEyesAgain () {
+
+    $(this).mousemove(function(event) {
+      var eye = $(".eye2");
+      var x = (eye.offset().left) + (eye.width() / 2);
+      var y = (eye.offset().top) + (eye.height() / 2);
+      var rad = Math.atan2(event.pageX - x, event.pageY - y);
+      var rot = (rad * (180 / Math.PI) * -1) + 180;
+      eye.css({
+        '-webkit-transform': 'rotate(' + rot + 'deg)',
+        '-moz-transform': 'rotate(' + rot + 'deg)',
+        '-ms-transform': 'rotate(' + rot + 'deg)',
+        'transform': 'rotate(' + rot + 'deg)'
+      });
+    });
+  };
+
+};
+
+//moveeyes();
 
 
 
@@ -298,13 +227,3 @@ moveeyes();
 
 
 });
-
-
-
-
-
-//BUTTON ready to add points if you click on reset
-// $("#button").click(function() {
-//   var getIncreasedValue = increment();
-//   $("#teamhedge").text(getIncreasedValue)
-// });
